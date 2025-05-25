0d512465e41c8b12be066cb405c4644bb7bc36d2
Luke Strickland <luke@axxim.net>
2025-05-25 09:44:31 -0400

    Adding CI test

--- test/query_canary_web/controllers/error_html_test.exs
+++ test/query_canary_web/controllers/error_html_test.exs
@@ -9,6 +9,7 @@ defmodule QueryCanaryWeb.ErrorHTMLTest do
   end
 
   test "renders 500.html" do
-    assert render_to_string(QueryCanaryWeb.ErrorHTML, "500", "html", []) == "Internal Server Error"
+    assert render_to_string(QueryCanaryWeb.ErrorHTML, "500", "html", []) ==
+             "Internal Server Error"
   end
 end
